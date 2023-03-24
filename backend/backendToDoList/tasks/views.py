16465a4fab8d1eb4d2c98931d10efa87a513185e
Allan <allan.davila@hotmail.com>
2023-03-23 19:26:04 -0700

    Added get function

--- backend/backendToDoList/tasks/views.py
+++ backend/backendToDoList/tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from tasks.models import Task
+from tasks.serializer import TaskSerializer# Create your views here.
+
+class TasksView(APIView):
+    def get(self,request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
