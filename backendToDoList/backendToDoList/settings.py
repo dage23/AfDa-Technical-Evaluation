5998f6be34e8b601ade7c027fed50336a0c53f6c
Allan <allan.davila@hotmail.com>
2023-03-27 14:11:00 -0700

    Added CORS to backend

--- backendToDoList/backendToDoList/settings.py
+++ backendToDoList/backendToDoList/settings.py
@@ -27,11 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
 
 # Application definition
 
 INSTALLED_APPS = [
     'tasks',
+    'corsheaders',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -49,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'backendToDoList.urls'
