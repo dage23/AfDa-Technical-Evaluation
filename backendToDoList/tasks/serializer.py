81310c962dd3bd1c80b729bf2fd7291af80c1a7c
Allan <allan.davila@hotmail.com>
2023-03-27 14:10:45 -0700

    Added ID to tasks json

--- backendToDoList/tasks/serializer.py
+++ backendToDoList/tasks/serializer.py
@@ -3,4 +3,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('title', 'completed')
+        fields = ('id','title', 'completed')
